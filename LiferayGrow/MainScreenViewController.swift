b7b26232a8ddb19d20a74223c73a2bce2da332f7
Vilmos Papp <vilmos.papp@liferay.com>
2016-09-06 15:41:48 +0200

    Styling the app

--- LiferayGrow/MainScreenViewController.swift
+++ LiferayGrow/MainScreenViewController.swift
@@ -13,7 +13,40 @@ class MainScreenViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		excellenceButton.layer.cornerRadius = 10
+		excellenceButton.layer.borderColor = UIColor.whiteColor().CGColor
+		excellenceButton.layer.borderWidth = 2
+
+		learnButton.layer.cornerRadius = 10
+		learnButton.layer.borderColor = UIColor.whiteColor().CGColor
+		learnButton.layer.borderWidth = 2
+
+		logoutButton.layer.cornerRadius = 10
+		logoutButton.layer.borderColor = UIColor.whiteColor().CGColor
+		logoutButton.layer.borderWidth = 2
+
+		peopleButton.layer.cornerRadius = 10
+		peopleButton.layer.borderColor = UIColor.whiteColor().CGColor
+		peopleButton.layer.borderWidth = 2
+
+		shareButton.layer.cornerRadius = 10
+		shareButton.layer.borderColor = UIColor.whiteColor().CGColor
+		shareButton.layer.borderWidth = 2
 		
+		UIGraphicsBeginImageContext(self.view.frame.size)
+		UIImage(named: "background.jpg")?.drawInRect(self.view.bounds)
+		
+		let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+		
+		UIGraphicsEndImageContext()
+		
+		self.view.backgroundColor = UIColor(patternImage: image)
+
+		self.navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+		self.navigationController!.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+		self.navigationController!.navigationBar.shadowImage = UIImage()
+		//self.navigationController!.navigationBar.translucent = true
+		self.navigationController!.view.backgroundColor = UIColor.clearColor()
 	}
 
 	@IBAction func logout(sender: AnyObject) {
@@ -23,4 +56,13 @@ class MainScreenViewController: UIViewController {
 		self.performSegueWithIdentifier("ShowHomeScreen", sender: nil)
 	}
 
+	@IBOutlet weak var peopleButton: UIButton!
+	@IBOutlet weak var learnButton: UIButton!
+	@IBOutlet weak var excellenceButton: UIButton!
+	@IBOutlet weak var shareButton: UIButton!
+	@IBOutlet weak var logoutButton: UIButton!
+	@IBOutlet var mainView: UIView!
+	
+	
+	
 }
---
+++ LiferayGrow/Readme.md
@@ -0,0 +1,6 @@
+Background image:
+
+Author: Pacheco
+Title: Christmas Starfish Love and The year of the 10th Fizzle, Malibu Pier
+Autrhor's website: www.pachecolandscapes.com
+License: https://creativecommons.org/licenses/by-nd/2.0/
